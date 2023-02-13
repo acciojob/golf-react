9e7e21981fd0079445f35b735f4a3d2ad1a88c4d
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-02-13 21:30:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
+		    return <button onClick={this.buttonClickHandler} >Start</button>
 		}
     }
 
